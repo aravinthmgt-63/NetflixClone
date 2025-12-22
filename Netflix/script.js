c453f17304c71b8c1844705ed796ecb0fec31c12
Aravinth63 <mgtaravinth71@gmail.com>
2025-12-22 05:28:13 -0800

    Delete Netflix directory

--- Netflix/script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("scroll", () => {
-    const nav = document.querySelector(".navbar");
-    if (window.scrollY > 80) {
-        nav.style.background = "#000";
-    } else {
-        nav.style.background = "rgba(0,0,0,0.75)";
-    }
-});
